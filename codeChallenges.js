5db3185d974aba07b25df99139f1091f2b0e5313
Tristan Young <tyoung22@turing.edu>
2023-01-11 13:13:43 -0800

    Create a file to submit medium coding challenge

--- codeChallenges.js
+++ codeChallenges.js
@@ -1,10 +1,14 @@
-```javascript
-var goodDrivingRecord = true;
-var age = 24;
-```
-var goodDrivingRecord = true;
+
+var goodDrivingRecord = false;
 var age = 24;
 
+if (goodDrivingRecord ===true && age > 25) {
+    console.log ("You will recieve a discount on your car rental!")
+} else if (goodDrivingRecord === true || age > 25) {
+    console.log ("You have to pay full price on your car rental! ")
+} else if (goodDrivingRecord !== true && age < 25 ) {
+    console.log ("You need to have someone else sign for your car rental.")
+}
 
 // Write a program that evaluates the status of the variables above, and uses logic to print one of three possible messages:
 // - If the driving record is good and user is over 25 years old, they should get a discount on the car rental
