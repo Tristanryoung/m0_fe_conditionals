e824c1f0315aee4a66a104d701125ee48dfe2021
Tristan Young <tyoung22@turing.edu>
2023-01-11 11:11:55 -0800

    Run whether number of students is greater than or equal to 21

--- comparisons.js
+++ comparisons.js
@@ -43,6 +43,8 @@ console.log ("Is numberStudents greater than or equal to 20?", numberStudents >=
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
 
+console.log ("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
+
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
 
